9bcb69c1f6aff2e84c44dea5c1da49a63ab0fb1a
whickman1998 <wahickman@calpoly.edu>
2018-03-17 16:06:04 -0700

    added Weapon

---
+++ Weapon.java
@@ -0,0 +1,20 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Weapon extends JustActivityActions {
+    
+    public Weapon(Point position,
+                  List<PImage> images, int actionPeriod)
+    {
+        super(position, images, actionPeriod);
+    }
+    
+    public void executeActivity(WorldModel world,
+                                ImageStore imageStore, EventScheduler scheduler) {
+        world.removeEntity(this);
+        scheduler.unscheduleAllEvents(this);
+    }
+    
+}
+
